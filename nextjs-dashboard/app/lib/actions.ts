eeee968bef7f37d4b6efb1209319970b34dc0efa
lim <lbooks424@gmail.com>
2023-11-04 17:35:05 +0900

    [add] Validate and prepare the Invoice data

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -1,11 +1,31 @@
 'use server';
 
+import { z } from 'zod';
+
+const InvoiceSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = InvoiceSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  };
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
 
-  console.log(rawFormData);
+
+  console.log(typeof customerId);
+  console.log(typeof amount);
+  console.log(typeof status);
+
+  console.log(amountInCents + " cents");
+  console.log(date);
 }
