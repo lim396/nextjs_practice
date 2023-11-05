fe890af0c7053f90a20d80e51f04c1f355e0ffd5
lim <lbooks424@gmail.com>
2023-11-05 19:13:38 +0900

    [add] Display error messages in remaining fields

--- nextjs-dashboard/app/ui/invoices/create-form.tsx
+++ nextjs-dashboard/app/ui/invoices/create-form.tsx
@@ -70,9 +70,21 @@ const [state, dispatch] = useFormState(createInvoice, initialState);
                 step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                aria-describedby="customer-error"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
+            {state.errors?.amount ? (
+              <div
+                id="customer-error"
+                aria-live="polite"
+                className="mt-2 text-sm text-red-500"
+              >
+                {state.errors.amount.map((error: string) => (
+                  <p key={error}>{error}</p>
+                ))}
+              </div>
+            ) : null}
           </div>
         </div>
 
@@ -90,6 +102,7 @@ const [state, dispatch] = useFormState(createInvoice, initialState);
                   type="radio"
                   value="pending"
                   className="h-4 w-4 border-gray-300 bg-gray-100 text-gray-600 focus:ring-2 focus:ring-gray-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-gray-600"
+                  aria-describedby="customer-error"
                 />
                 <label
                   htmlFor="pending"
@@ -105,6 +118,7 @@ const [state, dispatch] = useFormState(createInvoice, initialState);
                   type="radio"
                   value="paid"
                   className="h-4 w-4 border-gray-300 bg-gray-100 text-gray-600 focus:ring-2 focus:ring-gray-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-gray-600"
+                  aria-describedby="customer-error"
                 />
                 <label
                   htmlFor="paid"
@@ -115,6 +129,26 @@ const [state, dispatch] = useFormState(createInvoice, initialState);
               </div>
             </div>
           </div>
+          {state.errors?.status ? (
+            <div
+              id="customer-error"
+              aria-live="polite"
+              className="mt-2 text-sm text-red-500"
+            >
+              {state.errors.status.map((error: string) => (
+                <p key={error}>{error}</p>
+              ))}
+            </div>
+          ) : null}
+          {state.message ? (
+            <div
+              id="customer-error"
+              aria-live="polite"
+              className="mt-2 text-sm text-red-500"
+            >
+              <p> {state.message} </p>
+            </div>
+          ) : null}
         </fieldset>
       </div>
       <div className="mt-6 flex justify-end gap-4">
